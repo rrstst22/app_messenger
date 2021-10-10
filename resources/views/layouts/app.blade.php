b577ceafd679b1d46abafd45c557fc213305fece
rrstst22 <tsrrryou@gmail.com>
2021-10-10 17:45:34 +0900

    fixed vue.js problem

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/basic.js') }}"></script>
 
 </body>
